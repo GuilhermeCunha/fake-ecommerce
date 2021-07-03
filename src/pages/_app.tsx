04eac4993e6068f068000e0f784b1578e17d8af2
Guilherme <guiscunha@gmail.com>
2021-07-03 10:17:53 -0300

    feat: adding empty project with TS and Tailwind

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+import React from "react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
