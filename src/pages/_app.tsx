95759b60c0d2324f61e812f81a310e8a94d83bef
Guilherme <guiscunha@gmail.com>
2021-07-05 12:00:05 -0300

    feat: adding contexts and tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
-import { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
+import { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 import 'antd/dist/antd.css';
 import '../styles/global.css';
 
+import React from 'react';
+import { SearchProvider } from 'contexts/SearchContext';
+import { AuthProvider } from 'contexts/AuthenticationContext';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <SearchProvider>
+        <Component {...pageProps} />
+      </SearchProvider>
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
