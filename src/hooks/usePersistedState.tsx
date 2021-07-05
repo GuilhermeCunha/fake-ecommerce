512159fd28d774ff538b5794fa0d777760b63e99
Guilherme <guiscunha@gmail.com>
2021-07-05 11:59:31 -0300

    feat: adding usePersistedState

--- src/hooks/usePersistedState.tsx
+++ src/hooks/usePersistedState.tsx
@@ -1,20 +1,24 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 export function usePersistedState<T>(
-  key: string,
-  defaultValue: T
+  defaultValue: T,
+  key?: string,
 ): [T, Dispatch<SetStateAction<T>>, boolean] {
   const [isLoading, setIsLoading] = useState(true);
   const [state, setState] = useState<T>();
 
   useEffect(() => {
-    setState(JSON.parse(localStorage.getItem(key)) || defaultValue);
-    setIsLoading(false);
+    if (key) {
+      setState(JSON.parse(localStorage.getItem(key)) || defaultValue);
+      setIsLoading(false);
+    }
   }, [key]);
 
   useEffect(() => {
-    if (!isLoading) localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+    if (key) {
+      if (!isLoading) localStorage.setItem(key, JSON.stringify(state));
+    }
+  }, [key, state, isLoading]);
 
   return [state, setState, isLoading];
 }
